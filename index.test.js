fbbb0fb8cf72770bdfb3ef3eb76483bdb7ffc53b
Mohamed B <mohamed@jellyfish-commuting.com>
2020-03-29 14:29:27 +0200

    Update tests

--- index.test.js
+++ index.test.js
@@ -5,5 +5,5 @@ describe('Soundex', () => {
   it('expect equal for Kabyle & Kapile', () => expect(soundex('Kabyle')).toBe(soundex('Kapile')));
   it('expect equal for Robert & Rupert', () => expect(soundex('Robert')).toBe(soundex('Rupert')));
   it('expect equal for Ashcraft & Ashcroft', () => expect(soundex('Ashcraft')).toBe(soundex('Ashcroft')));
-  it('expect different for one & two', () => expect(soundex('one')).not.toBe(soundex('two')));
+  it('expect different for Kabyle & Arab', () => expect(soundex('Kabyle')).not.toBe(soundex('Arab')));
 });
