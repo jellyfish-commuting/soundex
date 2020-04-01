178a886d9005425543061c11e2e3daae1fea7b8e
Mohamed B <mohamed@jellyfish-commuting.com>
2020-04-01 20:33:49 +0200

    Improve perf

--- index.js
+++ index.js
@@ -1,47 +1,50 @@
 // Letter mapping
 const codes = {
-  B: '1', F: '1', P: '1', V: '1',
-  C: '2', G: '2', J: '2', K: '2', Q: '2', S: '2', X: '2', Z: '2',
-  D: '3', T: '3',
-  L: '4',
-  M: '5', N: '5',
-  R: '6',
+  B: 1, F: 1, P: 1, V: 1,
+  C: 2, G: 2, J: 2, K: 2, Q: 2, S: 2, X: 2, Z: 2,
+  D: 3, T: 3,
+  L: 4,
+  M: 5, N: 5,
+  R: 6,
+  A: 0, E: 0, I: 0, O: 0, U: 0,
 };
 
 //-------
 // Soundex
 //-------
 module.exports = function (str) {
-  // Uppercase to match letter according mapping
-  const buffer = String(str).trim().toUpperCase();
-
-  // Empty ?
-  if (!buffer.length) {
-    return '0000';
-  }
+  // Uppercase
+  const buffer = String(str).toUpperCase();
 
   // Init result
-  let result = buffer.charAt(0) + (codes[buffer.charAt(1)] || '');
+  let result = buffer.charAt(0);
+  let previous = codes[result];
 
   // Map all chars
-  for(let i = 2; i < buffer.length; i += 1) {
+  for(let i = 1; i < buffer.length; i += 1) {
     // Init var
-    const code = codes[buffer.charAt(i)];
-    const previous = buffer.charAt(i - 1);
+    const letter = buffer.charAt(i);
+    const code = codes[letter];
 
-    // Should ignore letter ?
-    if(
-      // If letter has equal code than previous ?
-      !code || code === codes[previous]
-
-      // Also, apply rule: "two letters with the same number separated by 'h' or 'w' are coded as a single number"
-      || (((previous === 'H' || previous === 'W') && code === codes[buffer.charAt(i - 2)]))
-    ) {
+    // Ignore non letter character and apply rule: "two letters with the same number separated by 'h' or 'w' are coded as a single number"
+    if (code === undefined || letter === 'H' || letter === 'W') {
       continue;
     }
 
-    result += code;
+    // Append code if different than previous
+    if(code && code !== previous) {
+      result += code;
+    }
+
+    // Soundex key is fulfilled ?
+    if (result.length === 4) {
+      return result;
+    }
+
+    // Save previous for next loop
+    previous = code;
   }
 
-  return result.slice(0, 4).padEnd(4, '0');
+  // Fill with 0 if less than four chars
+  return result.padEnd(4, '0');
 };
