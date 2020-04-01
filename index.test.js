483999364bbecdabc163cda4e36d125828db96e8
Mohamed B <mohamed@jellyfish-commuting.com>
2020-04-01 18:59:51 +0200

    Update test cases

--- index.test.js
+++ index.test.js
@@ -1,9 +1,34 @@
 const soundex = require('./index');
 
+const samples = [
+  ["Ashcraft", "Ashcroft"],
+  ["Burroughs", "Burrows"],
+  ["D-day", "d jay"],
+  ["Dracula", "Drakula"],
+  ["Ekzampul", "example"],
+  ["Ellery", "Euler"],
+  ["Gauss", "Ghosh"],
+  ["Heilbronn", "Hilbert"],
+  ["Johnny", "Jonny"],
+  ["Kant", "Knuth"],
+  ["Ladd", "Lloyd"],
+  ["Lissajous", "Lukasiewicz"],
+  ["Moskowitz", "Moskovitz"],
+  ["O'Conner", "O'Connor"],
+  ["Robert", "Rupert"],
+  ["Soundex", "sownteks"],
+];
+
 // Start tests ...
 describe('Soundex', () => {
-  it('expect equal for Kabyle & Kapile', () => expect(soundex('Kabyle')).toBe(soundex('Kapile')));
-  it('expect equal for Robert & Rupert', () => expect(soundex('Robert')).toBe(soundex('Rupert')));
-  it('expect equal for Ashcraft & Ashcroft', () => expect(soundex('Ashcraft')).toBe(soundex('Ashcroft')));
+  samples.forEach(sample => it(
+    `expect equal for ${sample[0]} & ${sample[1]}`,
+    () => expect(soundex(sample[0])).toBe(soundex(sample[0])),
+  ));
+
+  // Common error case, many soundex return A226 instead of A261
+  it('expect A261 for Ashcraft', () => expect(soundex('Ashcraft')).toBe('A261'));
+
+  // Different case
   it('expect different for Kabyle & Arab', () => expect(soundex('Kabyle')).not.toBe(soundex('Arab')));
 });
