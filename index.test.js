54e18013a89a7247acb72ee1861bcb8b7ae7015e
Mohamed B <mohamed@jellyfish-commuting.com>
2020-05-28 13:29:16 +0200

    Update test cases

--- index.test.js
+++ index.test.js
@@ -3,7 +3,6 @@ const soundex = require('./index');
 const samples = [
   ["Ashcraft", "Ashcroft"],
   ["Burroughs", "Burrows"],
-  ["D-day", "d jay"],
   ["Dracula", "Drakula"],
   ["Ekzampul", "example"],
   ["Ellery", "Euler"],
@@ -13,7 +12,6 @@ const samples = [
   ["Kant", "Knuth"],
   ["Ladd", "Lloyd"],
   ["Lissajous", "Lukasiewicz"],
-  ["Moskowitz", "Moskovitz"],
   ["O'Conner", "O'Connor"],
   ["Robert", "Rupert"],
   ["Soundex", "sownteks"],
@@ -23,7 +21,7 @@ const samples = [
 describe('Soundex', () => {
   samples.forEach(sample => it(
     `expect equal for ${sample[0]} & ${sample[1]}`,
-    () => expect(soundex(sample[0])).toBe(soundex(sample[0])),
+    () => expect(soundex(sample[0])).toBe(soundex(sample[1])),
   ));
 
   // Common error case, many soundex return A226 instead of A261
